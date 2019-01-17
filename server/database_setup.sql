db2ffecf87690add4df6004ecdf27517eab75066
Wolfgang Walter <apfelbutzen@apfelbaeumle.de>
2019-01-17 23:51:17 +0100

    make database code easier to update; consolidate trigger;
    * avoid deleting everything including data
    * use .. on insert or update ... => less trigger statements
    
    rename trigger and functions called by the triggers

--- server/database_setup.sql
+++ server/database_setup.sql
@@ -1,14 +1,18 @@
 -- kybernetik-abrechnung requires postgres >= 10.5
 
-drop owned by abrechnung cascade;
+-- drop owned by abrechnung cascade;
 
+do $$ begin
 create type user_role as enum ('admin', 'writer', 'reader', 'banned');
+exception
+when duplicate_object then null;
+end $$;
 
 -- used for the  all database tables 
-create sequence last_mod_seq_counter;
+create sequence if not exists last_mod_seq_counter;
 
 -- user list
-create table users (
+create table if not exists users (
 	id text primary key not null,
 	name text not null,
 	email text,
@@ -22,24 +26,26 @@ create table users (
 	last_mod_seq bigint not null
 );
 
-create function users_notify_trigger_callback() returns trigger as $$ declare begin
+create or replace function users_notify_trigger() returns trigger as $$ declare begin
 	perform pg_notify('users', '');
 	return NEW;
 end; $$ language plpgsql;
-create trigger users_insert_notify_trigger after insert on users execute procedure users_notify_trigger_callback();
-create trigger users_update_notify_trigger after update on users execute procedure users_notify_trigger_callback();
 
-create function users_seq_trigger_callback() returns trigger as $$ declare begin
+drop trigger if exists users_notify_trigger on users;
+create trigger users_notify_trigger after insert or update on users execute procedure users_notify_trigger();
+
+create or replace function users_seq_trigger() returns trigger as $$ declare begin
 	NEW.last_mod_seq := nextval('last_mod_seq_counter');
 	return NEW;
 end; $$ language plpgsql;
-create trigger users_insert_seq_trigger before insert on users for each row execute procedure users_seq_trigger_callback();
-create trigger users_update_seq_trigger before update on users for each row execute procedure users_seq_trigger_callback();
 
-create index users_last_mod_seq_index on users (last_mod_seq);
+drop trigger if exists users_seq_trigger on users;
+create trigger users_seq_trigger before insert or update on users for each row execute procedure users_seq_trigger();
+
+create index if not exists users_last_mod_seq_index on users (last_mod_seq);
 
 -- user authentication tokens
-create table auth_tokens (
+create table if not exists auth_tokens (
 	uid text not null,
 	device_id text not null,
 	token text not null,
@@ -48,14 +54,14 @@ create table auth_tokens (
 );
 
 -- when a user is added, or requests a password reset, he receives a pwreset mail with a token
-create table set_password_tokens (
+create table if not exists set_password_tokens (
 	uid text primary key not null references users (id),
 	token text not null,
 	sent timestamp not null default current_timestamp
 );
 
 -- group list
-create table groups (
+create table if not exists groups (
 	id serial primary key,
 	name text not null,
 	created_by text not null references users (id),
@@ -63,22 +69,24 @@ create table groups (
 	last_mod_seq bigint not null
 );
 
-create function groups_notify_trigger_callback() returns trigger as $$ declare begin
+create or replace function groups_notify_trigger() returns trigger as $$ declare begin
   perform pg_notify('groups', '');
   return NEW;
 end; $$ language plpgsql;
-create trigger groups_insert_notify_trigger after insert on groups execute procedure groups_notify_trigger_callback();
-create trigger groups_update_notify_trigger after update on groups execute procedure groups_notify_trigger_callback();
 
-create function groups_seq_trigger_callback() returns trigger as $$ declare begin
+drop trigger if exists groups_notify_trigger on groups;
+create trigger groups_notify_trigger after insert or update on groups execute procedure groups_notify_trigger();
+
+create or replace function groups_seq_trigger() returns trigger as $$ declare begin
 	NEW.last_mod_seq := nextval('last_mod_seq_counter');
 	return NEW;
 end; $$ language plpgsql;
-create trigger groups_insert_seq_trigger before insert on groups for each row execute procedure groups_seq_trigger_callback();
-create trigger groups_update_seq_trigger before update on groups for each row execute procedure groups_seq_trigger_callback();
+
+drop trigger if exists groups_seq_trigger on groups;
+create trigger groups_seq_trigger before insert or update on groups for each row execute procedure groups_seq_trigger();
 
 -- user/group membership
-create table group_memberships (
+create table if not exists group_memberships (
 	uid text references users (id),
 	gid integer not null references groups (id),
 	added timestamp not null default current_timestamp,
@@ -88,25 +96,27 @@ create table group_memberships (
 	last_mod_seq bigint not null
 );
 
-create index group_memberships_uid_index on group_memberships (uid);
-create index group_memberships_gid_index on group_memberships (gid);
+create index if not exists group_memberships_uid_index on group_memberships (uid);
+create index if not exists group_memberships_gid_index on group_memberships (gid);
 
-create function group_memberships_notify_trigger_callback() returns trigger as $$ declare begin
-  perform pg_notify('group_memberships', '');
-  return NEW;
+create or replace function group_memberships_notify_trigger() returns trigger as $$ declare begin
+	perform pg_notify('group_memberships', '');
+	return NEW;
 end; $$ language plpgsql;
-create trigger group_memberships_insert_notify_trigger after insert on group_memberships execute procedure group_memberships_notify_trigger_callback();
-create trigger group_memberships_update_notify_trigger after update on group_memberships execute procedure group_memberships_notify_trigger_callback();
 
-create function group_memberships_seq_trigger_callback() returns trigger as $$ declare begin
+drop trigger if exists group_memberships_notify_trigger on group_memberships;
+create trigger group_memberships_notify_trigger after insert or update on group_memberships execute procedure group_memberships_notify_trigger();
+
+create or replace function group_memberships_update_seq_trigger() returns trigger as $$ declare begin
 	NEW.last_mod_seq := nextval('last_mod_seq_counter');
 	return NEW;
 end; $$ language plpgsql;
-create trigger group_memberships_insert_seq_trigger before insert on group_memberships for each row execute procedure group_memberships_seq_trigger_callback();
-create trigger group_memberships_update_seq_trigger before update on group_memberships for each row execute procedure group_memberships_seq_trigger_callback();
+
+drop trigger if exists group_memberships_update_seq_trigger on group_memberships;
+create trigger group_memberships_update_seq_trigger before insert or update on group_memberships for each row execute procedure group_memberships_update_seq_trigger();
 
 -- patches which incrementally create the group objects
-create table patches (
+create table if not exists patches (
 	gid integer,
 	seq integer not null, -- the sequence number of the patch in the group.. within one gid, it is unique and unbroken. it starts from 0.
 	patch text,
@@ -119,19 +129,21 @@ create table patches (
 	last_mod_seq bigint not null
 );
 
-create index patches_gid_index on patches (gid);
-create index patches_seq_index on patches (seq);
+create index if not exists patches_gid_index on patches (gid);
+create index if not exists patches_seq_index on patches (seq);
 
-create function patches_notify_trigger_callback() returns trigger as $$ declare begin
+create or replace function patches_notify_trigger() returns trigger as $$ declare begin
   perform pg_notify('patches', '');
   return NEW;
 end; $$ language plpgsql;
-create trigger patches_insert_notify_trigger after insert on patches execute procedure patches_notify_trigger_callback();
-create trigger patches_update_notify_trigger after update on patches execute procedure patches_notify_trigger_callback();
 
-create function patches_seq_trigger_callback() returns trigger as $$ declare begin
+drop trigger if exists patches_notify_trigger on patches;
+create trigger patches_notify_trigger after insert or update on patches execute procedure patches_notify_trigger();
+
+create or replace function patches_seq_trigger() returns trigger as $$ declare begin
 	NEW.last_mod_seq := nextval('last_mod_seq_counter');
 	return NEW;
 end; $$ language plpgsql;
-create trigger patches_insert_seq_trigger after insert on patches for each row execute procedure patches_seq_trigger_callback();
-create trigger patches_update_seq_trigger after update on patches for each row execute procedure patches_seq_trigger_callback();
+
+drop trigger if exists patches_seq_trigger on patches;
+create trigger patches_seq_trigger after insert or update on patches for each row execute procedure patches_seq_trigger();

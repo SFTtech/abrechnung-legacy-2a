SFTtech/abrechnung-legacy-2a

commit 03a60ed59f12758f524794d10eff4f57787b3b16

allow more nodemailer auth methods to be configured
also allow no auth method at all

allow enforcement of TLS

server/mail.js
@@ -4,15 +4,22 @@ const nodemailer = require('nodemailer');
 
 const config = require('./config.js');
 
-const mail_transport = nodemailer.createTransport({
-    host: config.email.host,
-    port: config.email.port,
-    secure: (config.email.port == 465),
-    auth: {
-        user: config.email.user, // generated ethereal user
-        pass: config.email.password // generated ethereal password
+const email_config_to_nm_config = (ec) => {
+    const result = {
+        host: ec.host,
+        port: ec.port,
+        secure: (ec.port == 465),
+        requireTLS: !! ec.requireTLS
+    };
+    if (result.auth) {
+        result.auth = ec.auth;
     }
-});
+    return result;
+};
+
+const mail_transport = nodemailer.createTransport(
+    email_config_to_nm_config(config.email)
+);
 
 module.exports.send_mail = (uid, receiver, subject, text) => {
     if (receiver === null) {

commit fe8df4d07d71e7c0519379f5cee9e8f8accf88e3

server: do not ignore email.address configuration
Fixes: 03a60ed59f12758f524794d10eff4f57787b3b16

server/mail.js
@@ -8,6 +8,7 @@ const email_config_to_nm_config = (ec) => {
     const result = {
         host: ec.host,
         port: ec.port,
+        address: ec.address,
         secure: (ec.port == 465),
         requireTLS: !! ec.requireTLS
     };
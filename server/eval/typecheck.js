06dea630623a7871666ef1674b10a52e500e896c
Wolfgang Walter <apfelbutzen@apfelbaeumle.de>
2019-02-10 18:23:12 +0100

    server: typecheck: add check string_group_name

--- server/eval/typecheck.js
+++ server/eval/typecheck.js
@@ -21,6 +21,7 @@ module.exports.string_nz = (object) => (((typeof object) === "string") && (objec
 module.exports.string_re = (re) => (object) => (((typeof object) === "string") && object.match(re));
 module.exports.string_email = module.exports.string_re(/^[a-zA-Z0-9.+,-]+@[a-zA-Z0-9.-]+$/);
 module.exports.string_uid = module.exports.string_re(/^[a-zA-Z]([-]*[a-zA-Z0-9_])*$/);
+module.exports.string_group_name = module.exports.string_re(/^[a-zA-Z]([-]*[a-zA-Z0-9_])*$/);
 module.exports.choice = (set) => (object) => set.has(object);
 module.exports.optional = (inner_type) => (object) => ((object === undefined) || inner_type(object));
 module.exports.multicheck = (checks) => (object) => (checks.every( (check) => (check(object)) ));

SFTtech/abrechnung-legacy-2a

commit 998c0da6eef91e8dca2701f87b49514631399a59

db model: add column accepted to table group_membership

server/database_setup.sql
@@ -92,6 +92,7 @@ create table if not exists group_memberships (
 	added timestamp not null default current_timestamp,
 	added_by text not null references users (id),
 	role user_role not null,
+	acceped bool,
 	primary key (uid, gid),
 	last_mod_seq bigint not null
 );

commit 34842914eb8546bae82ff0a56d927a16cf1a752a

db: fix typo in table group_memberships
Fixes: 998c0da6eef91e8dca2701f87b49514631399a59 (db model: add column accepted to table group_membershi)

server/database_setup.sql
@@ -98,7 +98,7 @@ create table if not exists group_memberships (
 	added timestamp not null default current_timestamp,
 	added_by text not null references users (id),
 	role user_role not null,
-	acceped membership_acceptance not null default 'pending',
+	accepted membership_acceptance not null default 'pending',
 	primary key (uid, gid),
 	last_mod_seq bigint not null
 );